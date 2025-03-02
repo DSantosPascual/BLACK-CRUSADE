5bf2f57fe582fef16307df88a719416122048aac
DSantosPascual <d.santos.pascual@gmail.com>
2025-03-02 11:35:40 +0100

    cccccc

--- swagger.js
+++ swagger.js
@@ -12,23 +12,20 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000/api', // Usa el puerto de tu aplicación principal
+        url: 'http://localhost:3000/api', 
         description: 'Servidor local',
       },
     ],
   },
-  apis: ['./routes/apiRoutes.js'], // Ruta a tus archivos de rutas
+  apis: ['./routes/apiRoutes.js'],
 };
 
 const specs = swaggerJsdoc(options);
 
-// Crea un servidor Express separado para Swagger
 const swaggerApp = express();
 
-// Configura Swagger UI
 swaggerApp.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-// Inicia el servidor de Swagger en un puerto diferente
 const SWAGGER_PORT = 8000;
 swaggerApp.listen(SWAGGER_PORT, () => {
   console.log(`Swagger UI está corriendo en http://localhost:${SWAGGER_PORT}/api-docs`);
